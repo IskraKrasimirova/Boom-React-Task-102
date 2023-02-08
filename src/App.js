59edc0ac957215577c04897fe50e4fd42a27291d
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2023-02-08 23:00:28 +0200

    Add style

--- src/App.js
+++ src/App.js
@@ -4,24 +4,26 @@ import { motion } from "framer-motion/dist/framer-motion";
 
 function App() {
   return (
-    <motion.form
-      initial={{ x: '-200%' }}
-      animate={{ x: '0%' }}
-      transition={{ duration: 0.5 }}
-    >
-      <form>
-        <h1>Create An Account</h1>
-        <label>
-          Email:
-          <input name="email" type="email" required />
-        </label>
-        <label>
-          Password:
-          <input name="password" type="password" required />
-        </label>
-        <button>Sign Up!</button>
-      </form>
-    </motion.form>
+    <div className="App">
+      <motion.form
+        initial={{ x: '-200%' }}
+        animate={{ x: '0%' }}
+        style={{ position: 'absolute', top: '15%', transform: 'translateY(0%)' }}
+      >
+        <form>
+          <h1>Create An Account</h1>
+          <label>
+            Email:
+            <input name="email" type="email" required />
+          </label>
+          <label>
+            Password:
+            <input name="password" type="password" required />
+          </label>
+          <button>Sign Up!</button>
+        </form>
+      </motion.form>
+    </div>
   );
 }
 
